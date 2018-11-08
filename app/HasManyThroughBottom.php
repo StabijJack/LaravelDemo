86bb2860aa1d7dde3d10735c7fdd83f0c2c97dd1
StabijJack <jj.schneider@live.nl>
2018-11-08 12:38:36 +0100

    e

--- app/HasManyThroughBottom.php
+++ app/HasManyThroughBottom.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HasManyThroughBottom extends Model
 {
-    public function hasmanythroughmiddle(){
+    public function hasmanythroughmiddletotop(){
         return $this->belongsTo('App\HasManyThroughBottom','App\HasManyThroughMiddle');
     }    
 }
